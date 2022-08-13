638d7279f56d44a608da944dd0a816d54fb134b0
ms2br <ramazabqayev@gmail.com>
2022-08-13 17:40:13 +0400

    Initial commit

---
+++ Ödev - Araç Sınıfının Tanımlanması/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ödev___Araç_Sınıfının_Tanımlanması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleModels VehicleModelsAccess = new VehicleModels();
+            for (; ; )
+            {
+                Console.WriteLine("1.Ilan Ver");
+                Console.WriteLine("2.Fiyat Listesi");
+                Console.WriteLine("3.Cixis");
+                Console.Write("Seciniz : ");
+                int SelectValue = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (SelectValue)
+                {
+                    case 1:
+
+                        Console.Write("Model : ");
+                        string Model = Console.ReadLine();
+
+                        Console.Write("Marka : ");
+                        string Marka = Console.ReadLine();
+
+                        int ModelYear = VehicleModelsAccess.ModelYearsOversight();
+
+                        Console.Write("Kilometre : ");
+                        double Km = Convert.ToDouble(Console.ReadLine());
+
+                        VehicleModelsAccess = new VehicleModels(Model, Marka, ModelYear, Km);
+
+                        VehicleModelsAccess.FuelTypeOversight();
+
+                        Console.Write("Vites Tipi //Otomatik => true || Vites => false : ");
+                        bool _GearType = Convert.ToBoolean(Console.ReadLine().ToLower());
+                        VehicleModelsAccess.GearOversight(_GearType);
+
+                        Console.Write("Alis Fiyati : ");
+                        VehicleModelsAccess.PurchasePrice = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Satis Fiyati : ");
+                        VehicleModelsAccess.SalePrice = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Max Indirim Tutari : ");
+                        VehicleModelsAccess.MaxDiscountAmount = Convert.ToInt32(Console.ReadLine());
+
+                        VehicleModelsAccess.PriceOversight();
+
+                        Console.Clear();
+                        continue;
+
+                    case 2:
+                        Console.Clear();
+                        VehicleModelsAccess.ViewInformation();
+                        Thread.Sleep(3000);
+                        Console.Clear();
+                        continue;
+
+                    case 3:
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Yanlis Secim");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("Yeniden Seciniz");
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                        continue;
+                }
+                break;
+            }
+        }
+    }
+}
+
---
+++ Ödev - Araç Sınıfının Tanımlanması/VehicleModels.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ödev___Araç_Sınıfının_Tanımlanması
+{
+    class VehicleModels
+    {
+        public string Model;
+        public string Marka;
+        public int ModelYears;
+        public double Km;
+        public string FuelType;
+        public string GearType;
+        public int PurchasePrice;
+        public int SalePrice;
+        public int MaxDiscountAmount;
+        public int Price;
+
+        public VehicleModels()
+        {
+
+        }
+        public VehicleModels(string _Model, string _Marka)
+        {
+            Model = _Model;
+            Marka = _Marka;
+        }
+        public VehicleModels(string _Model, string _Marka, int _ModelYears)
+        {
+            Model = _Model;
+            Marka = _Marka;
+            ModelYears = _ModelYears;
+        }
+
+        public VehicleModels(string _Model, string _Marka, int _ModelYears, double _Km)
+        {
+            this.Model = _Model;
+            this.Marka = _Marka;
+            this.ModelYears = _ModelYears;
+            this.Km = _Km;
+        }
+
+        public void ViewInformation()
+        {
+            Console.WriteLine("Model : {0}", Model);
+            Console.WriteLine("Marka : {0}", Marka);
+            Console.WriteLine("Model Yili : {0}", ModelYears);
+            Console.WriteLine("Kilometre : {0}", Km);
+            Console.WriteLine("Yakit Tipi : {0}", FuelType);
+            Console.WriteLine("Vites Tipi : {0}", GearType);
+            Console.WriteLine("Alis Fiyati : {0}", PurchasePrice);
+            Console.WriteLine("Satis Fiyati : {0}", SalePrice);
+            Console.WriteLine("Max Indirim Tutari : {0}", MaxDiscountAmount);
+            Console.WriteLine("Son Fiyat : {0}", Price);
+        }
+
+        public void PriceOversight()
+        {
+            bool Oversight = false;
+            for (; Oversight == false;)
+            {
+                Console.Write("Son Fiyat : ");
+                Price = Convert.ToInt32(Console.ReadLine());
+                if (SalePrice - MaxDiscountAmount <= Price && Price <= SalePrice)
+                {
+                    Oversight = true;
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Girdiginiz Son Fitat Satis Fiyatindan Buyuk ve ya Indirm Fiyatini  Indirim Fiyatini Gectiniz");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                    Console.WriteLine("Yendiden Son Fiyati Giriniz");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                }
+            }
+        }
+
+        public bool GearOversight(bool _GearType)
+        {
+
+            switch (_GearType)
+            {
+                case true:
+                    GearType = "Otomatik";
+                    break;
+                case false:
+                    GearType = "Vites";
+                    break;
+            }
+            return false;
+        }
+
+        public int ModelYearsOversight()
+        {
+            int _ModelYear = 0;
+            bool YearsOversight = false;
+            for (; YearsOversight == false;)
+            {
+                Console.Write("Model Yili : ");
+                _ModelYear = Convert.ToInt32(Console.ReadLine());
+                if (_ModelYear <= DateTime.Now.Year)
+                {
+                    YearsOversight = true;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.Write("Girdigin Yil Cok Onde");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                }
+            }
+            return _ModelYear;
+        }
+
+        public void FuelTypeOversight()
+        {
+            Console.WriteLine("1.Dizel");
+            Console.WriteLine("2.Benzin");
+            Console.WriteLine("3.LPG ");
+            Console.WriteLine("4.Elektrik ");
+            while (true)
+            {
+                Console.Write("Yakit Tipi : ");
+                int _FuelType = Convert.ToInt32(Console.ReadLine());
+                switch (_FuelType)
+                {
+                    case 1:
+                        FuelType = "Dizel";
+                        break;
+                    case 2:
+                        FuelType = "Benzin";
+                        break;
+                    case 3:
+                        FuelType = "LPG";
+                        break;
+                    case 4:
+                        FuelType = "Elektrik";
+                        break;
+                    default:
+                        Console.WriteLine("Yanlis");
+                        continue;
+                }
+                break;
+            }
+        }
+    }
+}
